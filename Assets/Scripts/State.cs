45cf7ca0b5c74c8f00d4c419aa5bd434566f1674
Lorenzo Ortiz <jortiz3@rrc.ca>
2023-09-01 15:13:47 -0500

    Added base StateMachine scripts for the StateMachine framework. No included logic yet, mostly just comments on overall plans about it.

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class State : ScriptableObject
+{
+    // has type of states for evaluation??? (i.e. passive states always checking conditions, vs. active states where only 1 active at a time)
+    
+    // start condition
+    // end condition
+    // public Func<bool> Condition;
+    public List<StateCondition> Conditions { get { return _conditions; } }
+    [SerializeField] private List<StateCondition> _conditions = new List<StateCondition>();
+
+    public bool StateStatus { get; private set; } = false;
+
+    public void Initialize() => StateStatus = false;
+
+    // return true when logic ends.
+    public virtual bool DoState()
+    {
+        // only do state when all conditions are met
+        foreach (StateCondition condition in _conditions)
+            if (!condition.Evaluate()) return true;
+        return false;
+        // state executes logic either once or until a condition is met (i.e. while, do while)
+        // using an Action instead to check if states are finished
+    }
+
+    public abstract bool DoLogic();
+}
---
+++ Assets/Scripts/StateCondition.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public abstract class StateCondition : ScriptableObject
+{
+    public abstract bool Evaluate();
+}
