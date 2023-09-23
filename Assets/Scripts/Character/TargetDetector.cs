6f04c20a36da008723386098186411f1771da70f
Jose Lorenzo Ortiz <jortiz3@rrc.ca>
2023-09-23 00:42:56 -0500

    - Added wireframe for target detection.

--- Assets/Scripts/Character/TargetDetector.cs
+++ Assets/Scripts/Character/TargetDetector.cs
@@ -6,10 +6,17 @@ using System;
 public class TargetDetector : MonoBehaviour
 {
     public Action<CharacterAgent> OnPotentialTargetAcquired;
+    [SerializeField] private CircleCollider2D _collider;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         CharacterAgent agent = collision.GetComponent<CharacterAgent>();
         if (agent) OnPotentialTargetAcquired?.Invoke(agent);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, _collider.radius);
     }
 }
